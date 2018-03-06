2d6472fcb7557e6cc2617e76d6fac48d239fc2ce
Artem Shevchenko <developer.optimum@gmail.com>
2018-03-07 00:27:00 +0200

    save timetable positions

--- web/js/custom-bootstrap-table.js
+++ web/js/custom-bootstrap-table.js
@@ -1,10 +1,37 @@
 (function($) {
     'use strict';
 
-    var BootstrapTable = $.fn.bootstrapTable.Constructor;
+    var BootstrapTable = $.fn.bootstrapTable.Constructor,
+        _initBody = BootstrapTable.prototype.initBody,
+        _init = BootstrapTable.prototype.init,
+        _horizontalScroll = BootstrapTable.prototype.horizontalScroll,
+        _onColumnSearch = BootstrapTable.prototype.onColumnSearch
+    ;
+
+    BootstrapTable.prototype.init = function () {
+        _init.apply(this, Array.prototype.slice.apply(arguments));
+
+        var that = this;
+
+        this.$el.on('load-success.bs.table', function () {
+            if (localStorage.filterColumnsDefaults) {
+                var filterColumnsDefaults = JSON.parse(localStorage.filterColumnsDefaults);
+                that.$fixedHeader.find('input, select').each(function () {
+                    var field = $(this).closest('[data-field]').data('field');
+
+                    if (filterColumnsDefaults.hasOwnProperty(field)) {
+                        $(this).val(filterColumnsDefaults[field]);
+                    }
+                });
+                that.initColumnSearch(filterColumnsDefaults);
+                that.initSearch();
+                that.updatePagination();
+            }
+        });
+
+    };
 
     $.fn.bootstrapTable.methods.push('updateTimetable');
-
     BootstrapTable.prototype.updateTimetable = function (row) {
         var id = row.id || null;
         var customer_id = row.customer_id || null;
@@ -39,5 +66,74 @@
         this.initPagination();
         this.initSort();
         this.initBody(true);
+    };
+
+    BootstrapTable.prototype.initBody = function () {
+        _initBody.apply(this, Array.prototype.slice.apply(arguments));
+
+        $('.tt-tooltip').tooltip({
+            trigger: 'click',
+            container: 'body'
+        });
+
+        this.$tableBody.find('.times-comment').editable({
+            container: 'body',
+            display: function() {
+                return '';
+            },
+            success : function (response, newValue) {
+                if (newValue == '') {
+                    $(this).addClass('text-muted');
+                } else {
+                    $(this).removeClass('text-muted');
+                }
+            }
+        });
+
+        this.$tableBody.selectable({
+            filter: 'div.times-item'
+        });
+
+        $('.timetable td').hover(function() {
+            var index = $(this).index() + 1;
+            $('.timetable th:nth-child(' + index + ')').addClass('hover');
+        }, function() {
+            var index = $(this).index() + 1;
+            $('.timetable th:nth-child(' + index + ')').removeClass('hover');
+        });
+    };
+
+    BootstrapTable.prototype.horizontalScroll = function () {
+        _horizontalScroll.apply(this, Array.prototype.slice.apply(arguments));
+
+        this.$tableBody.on('scroll', function () {
+            localStorage.scrollTo = $(this).scrollTop();
+        });
+
+        if (localStorage.scrollTo) {
+            this.scrollTo(parseInt(localStorage.scrollTo));
+        }
+    };
+
+    BootstrapTable.prototype.onColumnSearch = function (event) {
+        _onColumnSearch.apply(this, Array.prototype.slice.apply(arguments));
+
+        var filterColumnsDefaults = {};
+        this.$fixedHeader.find('.form-control').each(function() {
+            var text = $.trim($(this).val());
+            var $field = $(this).closest('[data-field]').data('field');
+
+
+            if (text) {
+                filterColumnsDefaults[$field] = text;
+            }
+        });
+
+        if (filterColumnsDefaults) {
+            localStorage.filterColumnsDefaults = JSON.stringify(filterColumnsDefaults);
+        } else {
+            localStorage.removeItem('filterColumnsDefaults');
+        }
+
     };
 })(jQuery);
