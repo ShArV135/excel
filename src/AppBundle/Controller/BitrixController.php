9a62e558df6ed8a5509f603e06b660d6911ddac4
Artem Shevchenko <sharv135@gmail.com>
2021-02-24 23:27:31 +0200

    bitrix-api

---
+++ src/AppBundle/Controller/BitrixController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BitrixController extends Controller
+{
+    /**
+     * @Route("/bitrix-api", name="bitrix_api")
+     * @param LoggerInterface $logger
+     * @param Request         $request
+     * @return Response
+     */
+    public function apiAction(LoggerInterface $logger, Request $request): Response
+    {
+        $message = var_export($request->query->all() + $request->request->all());
+        $logger->info($message);
+
+        return new Response($message);
+    }
+}
